e6acc2fc3e06eb8e34e8d9845fd825c1d82adee1
navid khodaveisi <navidliverpool@yahoo.com>
2021-11-01 01:30:14 +0000

    Add project files.

---
+++ AlgorithmPracticeSumOfAllTheMultiplesOfinputNums/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmPracticeSumOfAllTheMultiplesOfinputNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
