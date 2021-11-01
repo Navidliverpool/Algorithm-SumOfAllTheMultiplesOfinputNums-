aac04d8a45d953bb570e59af81b4e3f3b41b77ac
navid khodaveisi <navidliverpool@yahoo.com>
2021-11-01 01:41:17 +0000

    third if statement done

--- AlgorithmPracticeSumOfAllTheMultiplesOfinputNums/Program.cs
+++ AlgorithmPracticeSumOfAllTheMultiplesOfinputNums/Program.cs
@@ -7,16 +7,21 @@ namespace AlgorithmPracticeSumOfAllTheMultiplesOfinputNums
         public static void Calculation(int firstInt, int secondInt, int value)
         {
             int count = 0;
-            for(int i = 0; i < value; i++)
+            for (int i = 0; i < value; i++)
             {
-                if(i % firstInt == 0 || i % secondInt == 0)
+                if (i % firstInt == 0 || i % secondInt == 0)
                 {
                     count += i;
                 }
-                else if(i % firstInt == 0 && i % secondInt != 0)
+                else if (i % firstInt == 0 && i % secondInt != 0)
                 {
                     count += i;
-                }            }
+                }
+                else if(i % secondInt == 0 && i % firstInt != 0)
+                {
+                    count += i;
+                }
+            }
         }
     }
 
