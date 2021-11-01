b61a449231bbc0bd04178c34aed85efdb641788e
navid khodaveisi <navidliverpool@yahoo.com>
2021-11-01 01:37:41 +0000

    raw for loop created

--- AlgorithmPracticeSumOfAllTheMultiplesOfinputNums/Program.cs
+++ AlgorithmPracticeSumOfAllTheMultiplesOfinputNums/Program.cs
@@ -4,9 +4,13 @@ namespace AlgorithmPracticeSumOfAllTheMultiplesOfinputNums
 {
     public static class Kata
     {
-        public static void Calculation(int value)
+        public static void Calculation(int firstInt, int secondInt, int value)
         {
+            int count = 0;
+            for(int i = 0; i < value; i++)
+            {
 
+            }
         }
     }
 
